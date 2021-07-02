80043a8201fe9693a51661cc9bc9341eb147b029
wangshangwen <wangshangwen>
2021-07-03 01:29:22 +0800

    add test script

--- test/dotenv.test.ts
+++ test/dotenv.test.ts
@@ -1,11 +1,27 @@
+// https://www.jestjs.cn/docs/expect
+
+import path from "path";
 import initEnv from "../src/dotenv";
 
 describe("Test Set Env", () => {
   it("Check development environment", () => {
     process.env.NODE_ENV = "development";
     initEnv({
+      cwd: path.join(process.cwd(), "example"),
       debug: true
     });
-    expect(true).toBeTruthy();
+    expect(process.env.LOCAL_VAR_A).toBe("a");
+    expect(process.env.LOCAL_VAR_B).toBe("B");
+    expect(process.env.DB_SECRET_KEY).toBe("dev_secret_key");
   });
+  // it("Check prouction environment", () => {
+  //   process.env.NODE_ENV = "production";
+  //   initEnv({
+  //     cwd: path.join(process.cwd(), "example"),
+  //     debug: true
+  //   });
+  //   expect(process.env.LOCAL_VAR_A).toBe("a");
+  //   expect(process.env.LOCAL_VAR_B).toBe("B");
+  //   expect(process.env.DB_SECRET_KEY).toBe("prod_secret_key");
+  // });
 });
